956c45a2918341cf8c2edce55857ae5c7ca78008
marcialuisa23 <marcia7monteiro@gmail.com>
2023-03-29 14:06:35 +0100

    completed unit conversion

--- src/app.js
+++ src/app.js
@@ -59,6 +59,8 @@ form.addEventListener("submit", handleSubmit);
 
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
@@ -67,12 +69,13 @@ function displayFahrenheitTemperature(event) {
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
-function displayCelsiusTemperature(event){
-    event.preventDefault();
-     let temperatureElement = document.querySelector("#temperature");
-     temperatureElement.innerHTML = celsiusTemperature;
-
-};
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = celsiusTemperature;
+}
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
